d328b85c11422f61bf77b4c452503f6b2371c5b6
kimsuheon1122 <kimsuheon1122@gmail.com>
2022-12-18 01:05:27 +0900

    차트추가

--- js/pfmain.js
+++ js/pfmain.js
@@ -29,5 +29,121 @@ setTimeout(function(){
 
 setTimeout(function(){
     main_cover[0].style["justify-content"] = 'flex-start'
+    document.querySelector("#nav").classList.remove("display_hidden");
 },8000)
+
+/* 그래프 그리기 */
+  const ctx = document.getElementById('myChart');
+const dataSet = [5, 10, 20, 40, 50, 60];
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['ABOUT ME', '개발기록&개발일기', 'SKILL', 'PROJECT', 'CONTACT'],
+      datasets: [{
+        data: dataSet,
+        borderWidth: 8,
+        borderColor: "#b7b7b7",
+        tension: 0.5,
+      }]
+    },
+    options: {
+        /* 몇번째 구간의 점을 클릭했는지 */
+        onClick : (e) => {
+            const canvasPosition = Chart.helpers.getRelativePosition(e, chart);
+            const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+
+            console.log('dataX',dataX);
+
+        },            
+        hoverRadius: 14,
+        hoverBackgroundColor: 'white',
+        plugins: {
+            tooltip: {
+                // Disable the on-canvas tooltip
+                enabled: false,
+
+                external: function(context) {
+                    // Tooltip Element
+                    let tooltipEl = document.getElementById('chartjs-tooltip');
+
+                    // Create element on first render
+                    if (!tooltipEl) {
+                        tooltipEl = document.createElement('div');
+                        tooltipEl.id = 'chartjs-tooltip';
+                        tooltipEl.innerHTML = '<table></table>';
+                        document.body.appendChild(tooltipEl);
+                    }
+
+                    // Hide if no tooltip
+                    const tooltipModel = context.tooltip;
+                    if (tooltipModel.opacity === 0) {
+                        tooltipEl.style.opacity = 0;
+                        return;
+                    }
+
+                    // Set caret Position
+                    tooltipEl.classList.remove('above', 'below', 'no-transform');
+                    if (tooltipModel.yAlign) {
+                        tooltipEl.classList.add(tooltipModel.yAlign);
+                    } else {
+                        tooltipEl.classList.add('no-transform');
+                    }
+
+                    function getBody(bodyItem) {
+                        return bodyItem.lines;
+                    }
+
+                    // Set Text
+                    if (tooltipModel.body) {
+                        const titleLines = tooltipModel.title || [];
+                        const bodyLines = tooltipModel.body.map(getBody);
+
+                        let innerHtml = '<thead>';
+
+                        titleLines.forEach(function(title) {
+                            innerHtml += '<tr><th>' + title + '</th></tr>';
+                        });
+                        innerHtml += '</thead>';
+
+                        bodyLines.forEach(function(body, i) {
+                            const colors = tooltipModel.labelColors[i];
+                            let style = 'background:' + colors.backgroundColor;
+                            style += '; border-color:' + colors.borderColor;
+                            style += '; border-width: 2px';
+                        });
+
+                        let tableRoot = tooltipEl.querySelector('table');
+                        tableRoot.innerHTML = innerHtml;
+                    }
+
+                    const position = context.chart.canvas.getBoundingClientRect();
+                    const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
+
+                    // Display, position, and set styles for font
+                    tooltipEl.style.opacity = 1;
+                    tooltipEl.style.position = 'absolute';
+                    tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX - 30 + 'px';
+                    tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + - 70 +'px';
+                    tooltipEl.style.font = bodyFont.string;
+                    tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
+                    tooltipEl.style.pointerEvents = 'none';
+                    tooltipEl.style.fontSize = "25px";
+
+                }
+            },
+            legend: false
+          },
+        interaction: {
+            mode: 'nearest',
+            intersect: false,
+            axis: 'x'
+          },
+          events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
+    },
+    xAxis: {
+        visible: false,
+    }
+  });
+
+ 
 
